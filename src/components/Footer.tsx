bd3210d687e2d01042ef1fd868a8014d1b56d6d8
Adebesin Olawale <olawaleadebesin@gmail.com>
2025-04-15 20:05:05 +0100

    developer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,15 @@ const Footer: React.FC = () => {
     <footer className="py-6 bg-gray-50">
       <div className="max-w-[100vw] mx-auto px-4 text-center">
         <p className="text-gray-600">
-          © {currentYear} Solanne Watches. All rights reserved.
+          © {currentYear} Solanne Watches. All rights reserved. | Developed by{' '}
+          <a 
+            href="https://github.com/codebygreenlight" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="text-amber-400 hover:text-amber-500 transition-colors duration-300"
+          >
+            devolawale
+          </a>
         </p>
       </div>
     </footer>
